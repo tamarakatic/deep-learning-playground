1619123bf82d9371ef89e372a45e27b8e68418de
Tamara Katic <tamarakatic@gmail.com>
2018-05-13 23:59:41 +0200

    Update script

--- boltzmann_machine/rbm.py
+++ boltzmann_machine/rbm.py
@@ -7,27 +7,27 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 
-movies = pd.read_csv('movie_lens_1m/movies.dat',
+movies = pd.read_csv('movie_lens1m/movies.dat',
                      sep='::',
                      header=None,
                      engine='python',
                      encoding='latin-1')
 
-users = pd.read_csv('movie_lens_1m/users.dat',
+users = pd.read_csv('movie_lens1m/users.dat',
                      sep='::',
                      header=None,
                      engine='python',
                      encoding='latin-1')
 
-ratings = pd.read_csv('movie_lens_1m/ratings.dat',
+ratings = pd.read_csv('movie_lens1m/ratings.dat',
                      sep='::',
                      header=None,
                      engine='python',
                      encoding='latin-1')
 
-training_set = pd.read_csv('movie_lens_100k/u1.base', delimiter='\t')
+training_set = pd.read_csv('movie_lens100k/u1.base', delimiter='\t')
 training_set = np.array(training_set, dtype='int')
-test_set = pd.read_csv('movie_lens_100k/u1.test', delimiter='\t')
+test_set = pd.read_csv('movie_lens100k/u1.test', delimiter='\t')
 test_set = np.array(test_set, dtype='int')
 
 nb_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
