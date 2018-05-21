ace41840fd5330d2b248d13c16167c058540c30c
Tamara Katic <tamarakatic@gmail.com>
2018-05-22 01:11:45 +0200

    Train the seq2seq model

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,105 +1,58 @@
 import numpy as np
 import time
-from preprocess import read_txt, clean_text
+from preprocess import read_txt, model_inputs
+from seq2seq import seq2seq_model, apply_padding
+from preprocess_answers_and_questions import preprocess_data
 
 import tensorflow as tf
 
 lines = read_txt('movie_lines.txt')
 conversations = read_txt('movie_conversations.txt')
 
-id2line = {}
-for line in lines:
-    _line = line.split(' +++$+++ ')
-    if len(_line) == 5:
-        id2line[_line[0]] = _line[4]
+epochs = 100
+batch_size = 64
+rnn_size = 512
+num_layers = 3
+encoding_embedding_size = 512
+decoding_embedding_size = 512
+learning_rate = 0.01
+learning_rate_decay = 0.9
+min_learning_rate = 0.0001
+keep_probability = 0.5
 
-conversations_ids = []
-for conversation in conversations[:-1]:
-    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
-    conversations_ids.append(_conversation.split(','))
+ans_words_to_int, ques_words_to_int, sort_clean_ques, sort_clean_ans = preprocess_data(lines, conversations)
 
-questions = []
-answers = []
-for conversation in conversations_ids:
-    for i in range(len(conversation) - 1):
-        questions.append(id2line[conversation[i]])
-        answers.append(id2line[conversation[i + 1]])
+tf.reset_default_graph()
+session = tf.InteractiveSession()       # Defining a session
 
+inputs, targets, learn_rate, keep_prob = model_inputs()
 
-clean_questions = []
-for question in questions:
-    clean_questions.append(clean_text(question))
+sequence_length = tf.placeholder_with_default(25, None, name='sequence_length')
 
-clean_answers = []
-for answer in answers:
-    clean_answers.append(clean_text(answer))
+input_shape = tf.shape(inputs)
 
-word2count = {}
-for question in clean_questions:
-    for word in question.split():
-        if word not in word2count:
-            word2count[word] = 1
-        else:
-            word2count[word] += 1
+training_predictions, test_predictions = seq2seq_model(tf.reverse(inputs, [-1]),
+                                                       targets,
+                                                       keep_prob,
+                                                       batch_size,
+                                                       sequence_length,
+                                                       len(ans_words_to_int),
+                                                       len(ques_words_to_int),
+                                                       encoding_embedding_size,
+                                                       decoding_embedding_size,
+                                                       rnn_size,
+                                                       num_layers,
+                                                       ques_words_to_int)
 
-for answer in clean_answers:
-    for word in answer.split():
-        if word not in word2count:
-            word2count[word] = 1
-        else:
-            word2count[word] += 1
+with tf.name_scope('optimization'):
+    loss_error = tf.contrib.seq2seq.sequence_loss(training_predictions,
+                                                  targets,
+                                                  tf.ones([input_shape[0], sequence_length]))
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    gradients = optimizer.compute_gradients(loss_error)
+    clipped_gradients = [(tf.clip_by_value(grad_tensor, -5., 5.), grad_variable)
+                         for grad_tensor, grad_variable in gradients
+                         if grad_tensor is not None]
+    optimizer_gradient_clipping = optimizer.apply_gradients(clipped_gradients)
 
-threshold = 20
-questions_words_to_int = {}
-word_number = 0
-for word, count in word2count.items():
-    if count >= threshold:
-        questions_words_to_int[word] = word_number
-        word_number += 1
-
-answers_words_to_int = {}
-word_number = 0
-for word, count in word2count.items():
-    if count >= threshold:
-        answers_words_to_int[word] = word_number
-        word_number += 1
-
-tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
-for token in tokens:
-    questions_words_to_int[token] = len(questions_words_to_int) + 1
-
-for token in tokens:
-    answers_words_to_int[token] = len(answers_words_to_int) + 1
-
-answers_ints_to_word = {w_i: w for w, w_i in answers_words_to_int.items()}
-
-for i in range(len(clean_answers)):
-    clean_answers[i] += ' <EOS>'
-
-questions_into_int = []
-for question in clean_questions:
-    ints = []
-    for word in question.split():
-        if word not in questions_words_to_int:
-            ints.append(questions_words_to_int['<OUT>'])
-        else:
-            ints.append(questions_words_to_int[word])
-    questions_into_int.append(ints)
-
-answers_into_int = []
-for answer in clean_answers:
-    ints = []
-    for word in answer.split():
-        if word not in answers_words_to_int:
-            ints.append(answers_words_to_int['<OUT>'])
-        else:
-            ints.append(answers_words_to_int[word])
-    answers_into_int.append(ints)
-
-sorted_clean_questions = []
-sorted_clean_answers = []
-for length in range(1, 25):
-    for i in enumerate(questions_into_int):
-        if len(i[1]) == length:
-            sorted_clean_questions.append(questions_into_int[i[0]])
-            sorted_clean_answers.append(answers_into_int[i[0]])
+training_validation_split = int(len(sort_clean_ques))
--- chatbot/preprocess.py
+++ chatbot/preprocess.py
@@ -1,10 +1,12 @@
 import re
 import codecs
 
+import tensorflow as tf
+
 
 def read_txt(path):
     return codecs.open(path, 'r', encoding='utf-8', errors='ignore').read().split('\n')
-
+    
 
 def clean_text(text):
     text = text.lower()
@@ -22,3 +24,18 @@ def clean_text(text):
     text = re.sub(r"can't", "cannot", text)
     text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
     return text
+
+
+def model_inputs():
+    inputs = tf.placeholder(tf.int32, [None, None], name='input')
+    targets = tf.placeholder(tf.int32, [None, None], name='target')
+    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    return inputs, targets, learning_rate, keep_prob
+
+
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0, 0], [batch_size, -1], [1, 1])
+    preprocessed_targets = tf.concat([left_side, right_side], axis=1)
+    return preprocessed_targets
