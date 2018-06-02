1765fbe9e5f8adeac753d6f8f07dcd6ce20d8eb9
Tamara Katic <tamarakatic@gmail.com>
2018-06-02 15:18:45 +0200

    Add training and testing part. Refactor code

--- chatbot/decoder.py
+++ chatbot/decoder.py
@@ -5,10 +5,11 @@ def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input,
                         sequence_length, decoding_scope, output_function,
                         keep_prob, batch_size):
     attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
-    attention_keys, attention_values, attention_score_fn, attention_construct_fn =
-        tf.contrib.seq2seq.prepare_attention(attention_states,
-                                             attention_option='bahdanau',
-                                             num_units=decoder_cell.output_size)
+    attention_keys, attention_values, attention_score_fn, \
+        attention_construct_fn = tf.contrib.seq2seq.prepare_attention(
+                                attention_states,
+                                attention_option='bahdanau',
+                                num_units=decoder_cell.output_size)
     dynamic_fn_train = tf.contrib.seq2seq.attention_decoder_fn_train(
                                                       encoder_state[0],
                                                       attention_keys,
@@ -29,10 +30,11 @@ def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix, sos_
                     eos_id, maximum_length, num_words, sequence_length, decoding_scope,
                     output_function, keep_prob, batch_size):
     attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
-    attention_keys, attention_values, attention_score_fn, attention_construct_fn =
-        tf.contrib.seq2seq.prepare_attention(attention_states,
-                                             attention_option='bahdanau',
-                                             num_units=decoder_cell.output_size)
+    attention_keys, attention_values, attention_score_fn, \
+        attention_construct_fn = tf.contrib.seq2seq.prepare_attention(
+                                attention_states,
+                                attention_option='bahdanau',
+                                num_units=decoder_cell.output_size)
     test_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_inference(
                                                           output_function,
                                                           encoder_state[0],
@@ -51,7 +53,7 @@ def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix, sos_
     return test_predictions
 
 
-def decoder_rnn(decoder_embedded_input, decoder_embeddedings_matrix, encoder_state,
+def decoder_rnn(decoder_embedded_input, decoder_embeddings_matrix, encoder_state,
                 num_words, sequence_length, rnn_size, num_layers, word2int, keep_prob,
                 batch_size):
     with tf.variable_scope("decoding") as decoding_scope:
@@ -76,7 +78,7 @@ def decoder_rnn(decoder_embedded_input, decoder_embeddedings_matrix, encoder_sta
                                                    batch_size)
         decoding_scope.reuse_variables()
         test_predictions = decode_test_set(encoder_state,
-                                           decoder_celll,
+                                           decoder_cell,
                                            decoder_embeddings_matrix,
                                            word2int[' <SOS> '],
                                            word2int[' <EOS> '],
